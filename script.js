a5f7d272aa80298d4bf963d936a00456e42b068e
Savannah Cody <savannah.m.cody@gmail.com>
2017-12-12 13:33:03 -0500

    finished putting the cards array into the JS file

--- script.js
+++ script.js
@@ -1,11 +1,59 @@
-//The game begins on a landing page with the title "The Golden Age of Television", the picture of the front of the first flashcard (compilation of show [pictures]) and the word "Start"
+var flashcards = [
+    {
+        Question: "What television show..........",
+        Answer: "Big Bang Theory",
+        cardImage: "[saved in project folder]"
+    },
+    {
+        Question: "What is the name of the huge movie and television streaming service that essentially put Blockbuster (if anybody remembers what that is) out of business in 2011. It was bought by Dish, who still has a few Blockbuster brick and mortar locations open",
+        Answer: "Netflix",
+        cardImage: "[saved in project folder]"
+    },
+    {
+        Question: "What Netflix show – its first original series – has recently had a dramatic character and story line shakeup due to the personal life of one of the main actors?",
+        Answer: "House of Cards",
+        cardImage: "[saved in project folder]"
+    },
+    {
+        Question: "What popular Hulu original show is based on the classic novel by Margaret Atwood and is the story of life in a totalitarian society in what was formerly the United States?",
+        Answer: "The Handmaids Tale",
+        cardImage: "[saved in project folder]"
+    },
+    {
+        Question: "What extremely [popular] NBC show includes flashbacks [as a way to tell the story] in every episode and is known for giving viewers “all the feels”?",
+        Answer: "This is Us",
+        cardImage: "[saved in project folder]"
+    },
+    {
+        Question: "What television show..........",
+        Answer: "Big Bang Theory",
+        cardImage: "[saved in project folder]"
+    },
+    {
+        Question: "What television show..........",
+        Answer: "Big Bang Theory",
+        cardImage: "[saved in project folder]"
+    },
+    {
+        Question: "What television show..........",
+        Answer: "Big Bang Theory",
+        cardImage: "[saved in project folder]"
+    }
+    ];
+// Example used from WDI pre-work Matching Game
 
-//Upon pushing the 'Start' button (located on/within 'Card1), 'Card1' will flip over to reveal 'question1' ""
+// The game begins on a landing page with the title "The Golden Age of Television", the picture of the front of the first flashcard (compilation of show [pictures]) and the word "Start"
 
-//The user will enter their answer into [the text input box]
-    //How to create a text input box
+// Upon 'click'ing the 'Start' button (located on/within 'Card1), 'Card1' will flip over to reveal 'question1' - ""
 
-//If the answer is correct, an [alert] will pop up saying "Correct Move on"
+// The user will enter their answer into [the text input box]
+    // How to create a text input box
 
-//If 'question1' is right, reveal the 'img' for 'question2' and 'question2' itself 
+// If the answer is correct, an [alert] will pop up saying "Correct, now click on the NEXT flashcard" 
 
+// If 'question1-question8' are incorrect, an [alert] will say "Wrong, try again", then disappear - reshowing the failed question
+
+// If the answer to any of 'question1'-'question8' is not correct, prevent it from allowing the next sequence to continue
+
+var question1answer = "Big Bang Theory"
+// This variable value that should go onto the back-side of the first card
