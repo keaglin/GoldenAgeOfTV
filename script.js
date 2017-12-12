40bc11bedcfd51857c599f3baeb3458a86288f51
Savannah Cody <savannah.m.cody@gmail.com>
2017-12-11 23:53:30 -0500

    added option buttons and pseudo code for js file

---
+++ script.js
@@ -0,0 +1,9 @@
+//The game begins on a landing page with the title "The Golden Age of Television", the picture of the front of the first flashcard (compilation of show [pictures]) and the word "Start"
+
+//Upon pushing the 'Start' button, 'Card1' will flip over to reveal 'question1' ""
+
+//The user will enter their answer in [the input box / choose from the option buttons]
+    //How to create 
+
+//If 'question1' is right, reveal the 'img' for 'question2' and 'question2' itself 
+
