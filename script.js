a0a8dc37d6a4ffd3875ac7596131edd049a2aa2b
Savannah Cody <savannah.m.cody@gmail.com>
2017-12-12 12:23:10 -0500

    updated html file and buttons, began adding JS, created array for cards

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 //The game begins on a landing page with the title "The Golden Age of Television", the picture of the front of the first flashcard (compilation of show [pictures]) and the word "Start"
 
-//Upon pushing the 'Start' button, 'Card1' will flip over to reveal 'question1' ""
+//Upon pushing the 'Start' button (located on/within 'Card1), 'Card1' will flip over to reveal 'question1' ""
 
-//The user will enter their answer in [the input box / choose from the option buttons]
-    //How to create 
+//The user will enter their answer into [the text input box]
+    //How to create a text input box
+
+//If the answer is correct, an [alert] will pop up saying "Correct Move on"
 
 //If 'question1' is right, reveal the 'img' for 'question2' and 'question2' itself 
 
