c6c404ecf7710c988b803453c01ff85baff43ee4
Savannah Cody <savannah.m.cody@gmail.com>
2017-12-14 14:05:00 -0500

    tested game and updated code based on results, managed to fix the issues

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ card1StartButton.addEventListener('click', function () {
          card1StartButton.setAttribute('disabled', true)
     } else {
         console.log("Wrong!")
-        alert("Wrong. Try again!")
+        alert("Wrong! Try again! Think rocket scientists....")
     }
     })
 
@@ -55,7 +55,7 @@ card2Button.addEventListener('click', function () {
         console.log(answer)
     if (answer === "Netflix") {
             console.log("Correct!")
-             alert("Correct! Avoid taking time to Netflix and chill, instead move on to card #3")
+             alert("You got it! Avoid taking time to Netflix and chill, instead move on to card #3")
              card2Button.setAttribute('disabled', true)
     } else {
             console.log("Wrong!")
@@ -70,7 +70,7 @@ card3Button.addEventListener('click', function () {
         console.log(answer)
     if (answer === "House of Cards") {
             console.log("Correct!")
-             alert("Correct! Move on to card #4")
+             alert("That's right! Keep on going")
              card3Button.setAttribute('disabled', true)
     } else {
             console.log("Wrong!")
@@ -87,7 +87,7 @@ card4Button.addEventListener('click', function () {
 
     if (answer === "The Handmaids Tale") {
             console.log("Correct - Peace be the Fruit!")
-             alert("Correct! Move on to card #5")
+             alert("Correct! Move on to the next card")
              card4Button.setAttribute('disabled', true)
     } else {
             console.log("Wrong!")
@@ -103,7 +103,7 @@ card5Button.addEventListener('click', function () {
         console.log(answer)
 
     if (answer === "This is Us") {
-            console.log("Correct! [TIU catch phrase]")
+            console.log("Correct!")
              alert("Correct! Move on to card #6")
              card5Button.setAttribute('disabled', true)
     } else {
@@ -120,12 +120,12 @@ card6Button.addEventListener('click', function () {
         console.log(answer)
 
     if (answer === "The Deuce") {
-            console.log("Correct! [TD catch phrase]")
-             alert(".button")
+            console.log("Right! That was unexpected...continue on to the next card")
+             alert("Right! That was unexpected...continue on to the next card")
              card6Button.setAttribute('disabled', true)
     } else {
             console.log("Wrong!")
-            alert("Wrong. Try again!")
+            alert("Wrong! Give it another shot.")
     }
 })
 
@@ -138,12 +138,12 @@ card7Button.addEventListener('click', function () {
         console.log(answer)
 
     if (answer === "Insecure") {
-            console.log("Correct! [I catch phrase]")
-             alert("Correct! Move on to card #7")
+            console.log("Correct!")
+             alert("You got it!! You know your HBO shows dont you? This last one should be easy...")
              card7Button.setAttribute('disabled', true)
     } else {
             console.log("Wrong!")
-            alert("Wrong. Try again!")
+            alert("Wrong! Try again. You're almost at the end!")
     }
 })
 
@@ -155,11 +155,11 @@ card8Button.addEventListener('click', function () {
         console.log(answer)
 
     if (answer === "Game of Thrones") {
-            console.log("Correct! [Winter catch phrase]")
-             alert("Correct! Move on to card #7")
+            console.log("Correct!")
+             alert("Yay! You Win!!!! (aka you spend a LOT of time watching tv :-))")
              card8Button.setAttribute('disabled', true)
     } else {
             console.log("Wrong!")
-            alert("Wrong. Try again!")
+            alert("Wrong! Looks like winter came for just you... Try again!")
     }
 })
